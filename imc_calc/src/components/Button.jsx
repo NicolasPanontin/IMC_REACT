42e847571ac049ebc05dafef4761a35dc99ff20f
NicolasPanontin <115738875+NicolasPanontin@users.noreply.github.com>
2023-10-04 12:02:06 -0300

    first commit

---
+++ imc_calc/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import "./Button.css";
+
+const button = ({id, text, action }) => {
+  const handleAction = (e) => {
+    action(e)
+  }
+
+  return (
+  <button id={id} onClick={handleAction}>{text}</button>
+  );
+};
+
+export default button
